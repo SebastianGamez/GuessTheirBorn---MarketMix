8342db1dcababc8567cdf6a04bcf4086e72b842d
Sebastian Gamez Ariza <juan.gamez1001@gmail.com>
2023-02-06 17:22:57 -0500

    Finalización a priori de la lógica

--- src/main.js
+++ src/main.js
@@ -1,46 +1,75 @@
 const { createApp } = Vue;
 
-
 const app = createApp({
     data() {
         return {
+
+            render: {
+                login: true,
+                game: false,
+                results: false
+            },
 
             attempts: 0,
 
             bornToGuess: '',
             
-            formData: {
-                name: '',
-                bornDate: ''
-            },
+            nameInput: '',
             
-            bornInput: ''
+            bornInput: '',
+
+            results: []
         }
     },
+
+    mounted() {
+        this.results = localStorage.getItem('results') ? JSON.parse(localStorage.getItem('results')) : [];
+    },
+
     methods: {
         addUser(){
             bornDatesData.push(formData);
             swal("Usuario", "Usuario registrado con exito", "success");
         },
+        getRandomNumber(){
+            return Math.floor(Math.random() * 4);
+        },
         getRandomBornDate(){
-            return bornDatesData[Math.floor(Math.random() * (bornDatesData.length() - 1))].bornDate;
+            return bornDatesData[Math.floor(Math.random() * (bornDatesData.length()))].bornDate.getFullYear();
         },
         startPlay(){
 
-            ++attempts;
-
-            this.bornToGuess = this.bornToGuess === '' ? this.getRandomBornDate() : this.bornToGuess;
-
-            const bornSelected = new Date(this.bornInput)
-
-            if(this.bornToGuess < bornSelected){
-                swal("Usuario", "La fecha seleccionada es mayor", "warning");
-            } else if(this.bornToGuess > bornSelected){
-                swal("Usuario", "La fecha seleccionada es menor", "warning");
-            } else {
-                swal("Usuario", "Felicitaciones, la fecha seleccionada es correcta", "success");
+            if(this.attempts === 7) {
+                this.attempts = 0;
+                return swal("Usuario", "Se acabaron los intentos", "error");
             }
 
+            ++this.attempts;
+
+            this.bornToGuess = this.attempts === 1 ? this.getRandomBornDate() : this.bornToGuess;
+
+            const bornSelected = this.bornInput
+
+            if(this.bornToGuess < bornSelected){
+                swal("Usuario", `La fecha seleccionada es mayor, intenta con un año entre ${this.bornToGuess - this.getRandomNumber} y ${this.bornToGuess + this.getRandomNumber}`, "warning");
+            } else if(this.bornToGuess > bornSelected){
+                swal("Usuario", `La fecha seleccionada es menor, intenta con un año entre ${this.bornToGuess - this.getRandomNumber} y ${this.bornToGuess + this.getRandomNumber}`, "warning");
+
+            } else {
+                swal("Usuario", "Felicitaciones, la fecha seleccionada es correcta", "success");
+                this.results.push({
+                    name: this.formData.name,
+                    attempts: this.attempts
+                });
+                localStorage.setItem('results', JSON.stringify(this.results));
+            }
+
+        },
+        backLogin(){
+            this.attempts = 0;
+            this.render.login = true;
+            this.render.game = false;
+            this.render.results = false;
         }
     }
 });
